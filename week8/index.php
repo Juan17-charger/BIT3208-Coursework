d26bf1d5824bf0353b2be81d5538ef58f5a30b5d
Juan17-charger <juansomo08@gmail.com>
2026-06-28 21:30:45 +0300

    Initial commit - BIT3208 coursework

---
+++ week8/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 8 Responsive Website</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+   <header>
+    <h1>Juan Tech Store</h1>
+</header>
+
+<nav>
+    <a href="#">Home</a>
+    <a href="#">Products</a>
+    <a href="#">Categories</a>
+    <a href="#">Contact</a>
+</nav>
+
+<section class="hero">
+    <h2>Welcome to Juan Tech Store</h2>
+    <p>Find the best laptops, phones and accessories.</p>
+
+    <button>Shop Now</button>
+</section>
+
+<main>
+
+    <div class="card">
+
+        <img src="images/product1.jpg" alt="Laptop">
+
+        <h2>RayBan Meta Glasses</h2>
+
+        <p>KES 95,000</p>
+
+        <button>Add to Cart</button>
+
+    </div>
+
+    <div class="card">
+
+        <img src="images/product2.jpg" alt="Phone">
+
+        <h2>Smart Watch</h2>
+
+        <p>KES 45,000</p>
+
+        <button>Add to Cart</button>
+
+    </div>
+
+    <div class="card">
+
+        <img src="images/product3.jpg" alt="Headphones">
+
+        <h2>Wireless Headphones</h2>
+
+        <p>KES 8,500</p>
+
+        <button>Add to Cart</button>
+
+    </div>
+
+</main>
+
+<footer>
+
+    <p>&copy; 2026 Juan Tech Store. All Rights Reserved.</p>
+
+</footer>
+
+</body>
+</html>
